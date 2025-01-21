1ed024a6a8dba46c20cc2389ddff442423cbecbe
Darya Babich <darya.babich11@gmail.com>
2025-01-21 16:56:33 +0300

    fix: remove file reading in subfolders

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,30 +1,22 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
+
 const secretFolderPath = path.join(__dirname, 'secret-folder');
 
-async function readFilesInFolder(folderPath) {
-  try {
-    const files = await fs.readdir(folderPath, { withFileTypes: true });
-
-    for (const file of files) {
-      const fullPath = path.join(folderPath, file.name);
-
-      if (file.isDirectory()) {
-        await readFilesInFolder(fullPath);
-      } else {
-        const stats = await fs.stat(fullPath);
-        const fileSize = stats.size / 1024;
-        const fileName = path.parse(file.name).name;
-        const fileExtension = path.extname(file.name).slice(1);
-        const outputName = file.name.startsWith('.')
-          ? `- ${file.name}`
-          : `${fileName} - ${fileExtension}`;
-
-        console.log(`${outputName} - ${fileSize.toFixed(3)}kb`);
-      }
-    }
-  } catch (err) {
+fs.readdir(secretFolderPath, { withFileTypes: true }, (err, files) => {
+  if (err) {
     console.error('Error:', err);
+    return;
   }
-}
-readFilesInFolder(secretFolderPath);
+
+  files.forEach((file) => {
+    if (file.isFile()) {
+      const filePath = path.join(secretFolderPath, file.name);
+      const fileSize = fs.statSync(filePath).size / 1024;
+      const fileName = path.parse(file.name).name;
+      const fileExtension = path.extname(file.name).slice(1);
+
+      console.log(`${fileName} - ${fileExtension} - ${fileSize.toFixed(3)}kb`);
+    }
+  });
+});
