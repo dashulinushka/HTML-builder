04734f2301f82be8336365805923426745d3469b
Darya Babich <darya.babich11@gmail.com>
2025-01-22 11:56:02 +0300

    fix: remove sync-method in task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,22 +1,28 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const secretFolderPath = path.join(__dirname, 'secret-folder');
 
-fs.readdir(secretFolderPath, { withFileTypes: true }, (err, files) => {
-  if (err) {
-    console.error('Error:', err);
-    return;
-  }
+const displayFilesInfo = async () => {
+  try {
+    const files = await fs.readdir(secretFolderPath, { withFileTypes: true });
 
-  files.forEach((file) => {
-    if (file.isFile()) {
-      const filePath = path.join(secretFolderPath, file.name);
-      const fileSize = fs.statSync(filePath).size / 1024;
-      const fileName = path.parse(file.name).name;
-      const fileExtension = path.extname(file.name).slice(1);
+    for (const file of files) {
+      if (file.isFile()) {
+        const filePath = path.join(secretFolderPath, file.name);
+        const stats = await fs.stat(filePath);
+        const fileSize = stats.size / 1024;
+        const fileName = path.parse(file.name).name;
+        const fileExtension = path.extname(file.name).slice(1);
 
-      console.log(`${fileName} - ${fileExtension} - ${fileSize.toFixed(3)}kb`);
+        console.log(
+          `${fileName} - ${fileExtension} - ${fileSize.toFixed(3)}kb`,
+        );
+      }
     }
-  });
-});
+  } catch (err) {
+    console.error('Error:', err);
+  }
+};
+
+displayFilesInfo();
