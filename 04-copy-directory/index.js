785664e3287e2c3119e17a22a0bdb7956276a67d
Darya Babich <darya.babich11@gmail.com>
2025-01-23 16:18:53 +0300

    fix: add unlink-method in task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -8,11 +8,19 @@ const copyDir = async () => {
   await fs.promises.mkdir(destDir, { recursive: true });
 
   const files = await fs.promises.readdir(sourceDir);
+  const destFiles = await fs.promises.readdir(destDir);
 
   for (const file of files) {
     const srcFile = path.join(sourceDir, file);
     const destFile = path.join(destDir, file);
     await fs.promises.copyFile(srcFile, destFile);
+  }
+
+  for (const file of destFiles) {
+    if (!files.includes(file)) {
+      const destFile = path.join(destDir, file);
+      await fs.promises.unlink(destFile);
+    }
   }
 };
 
